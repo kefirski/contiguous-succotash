7fc1adb58ae787a4e8d6aefe4aafbd5e038b66a0
davideturco <davide.turco@bristol.ac.uk>
2021-05-22 23:27:20 +0100

    Changed default params

--- utils/parameters.py
+++ utils/parameters.py
@@ -17,7 +17,7 @@ class Parameters:
         self.kernels = [(1, 25), (2, 50), (3, 75), (4, 100), (5, 125), (6, 150)]
         self.sum_depth = fold(lambda x, y: x + y, [depth for _, depth in self.kernels], 0)
 
-        self.encoder_rnn_size = 150
+        self.encoder_rnn_size = 256
         self.encoder_num_layers = 2
 
         self.latent_variable_size = 90
